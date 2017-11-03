a299da16be9500e8e38fd95f8ec5990bd0fb3b22
Terence <tlamcy@yahoo.com.sg>
2017-11-04 01:31:01 +0800

    f

--- vagrant.sh
+++ vagrant.sh
@@ -8,7 +8,7 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker vagrant
 sudo systemctl enable docker
 sudo systemctl start docker
 
